5ede85578a59f9085407fafde2d8a2c16563e56b
WombatCoder-KMH <96988828+WombatCoder-KMH@users.noreply.github.com>
2026-06-20 19:40:09 +0200

    Add log-only message configuration option

--- Project Files/DLLSources/CvCity.cpp
+++ Project Files/DLLSources/CvCity.cpp
@@ -5010,7 +5010,14 @@ void CvCity::calculateNetYields(int aiYields[NUM_YIELD_TYPES], int* aiProducedYi
 						if (!pyl.hasAnyYield() && bPrintWarning)
 						{
 							CvWString szBuffer = gDLL->getText("TXT_KEY_NO_RAW_ON_FIELD", getNameKey(),GC.getYieldInfo(eYieldProduced).getChar());
-							gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_DEAL_CANCELLED", MESSAGE_TYPE_MINOR_EVENT, GC.getYieldInfo(eYieldProduced).getButton(), COLOR_RED, true, true);
+
+							InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+							if (GC.getOUT_OF_RESOURECES_MESSAGES() == 0)
+							{
+								messageType = MESSAGE_TYPE_LOG_ONLY;
+							}
+
+							gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_DEAL_CANCELLED", messageType, GC.getYieldInfo(eYieldProduced).getButton(), COLOR_RED, true, true);
 						}
 					}
 				}
@@ -5316,7 +5323,14 @@ void CvCity::calculateNetYields(int aiYields[NUM_YIELD_TYPES], int* aiProducedYi
 					}
 				}
 				CvWString szBuffer = gDLL->getText("TXT_KEY_NO_RAW", getNameKey(),GC.getYieldInfo(eYieldConsumed).getChar(), GC.getYieldInfo(eYieldProduced).getChar());
-				gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_DEAL_CANCELLED", MESSAGE_TYPE_MINOR_EVENT, GC.getYieldInfo(eYieldConsumed).getButton(), COLOR_RED, true, true);
+
+				InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+				if (GC.getOUT_OF_RESOURECES_MESSAGES() == 0)
+				{
+					messageType = MESSAGE_TYPE_LOG_ONLY;
+				}
+
+				gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_DEAL_CANCELLED", messageType, GC.getYieldInfo(eYieldConsumed).getButton(), COLOR_RED, true, true);
 			}
 		}
 	}
@@ -5341,7 +5355,14 @@ void CvCity::calculateNetYields(int aiYields[NUM_YIELD_TYPES], int* aiProducedYi
 					}
 				}
 				CvWString szBuffer = gDLL->getText("TXT_KEY_ALMOST_NO_RAW", getNameKey(),GC.getYieldInfo(eYieldConsumed).getChar(), GC.getYieldInfo(eYieldProduced).getChar());
-				gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_DEAL_CANCELLED", MESSAGE_TYPE_MINOR_EVENT, GC.getYieldInfo(eYieldConsumed).getButton(), COLOR_RED, true, true);
+
+				InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+				if (GC.getALMOST_NO_RAW_MESSAGES() == 0)
+				{
+					messageType = MESSAGE_TYPE_LOG_ONLY;
+				}
+
+				gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_DEAL_CANCELLED", messageType, GC.getYieldInfo(eYieldConsumed).getButton(), COLOR_RED, true, true);
 			}
 		}
 	}
@@ -5364,7 +5385,14 @@ void CvCity::calculateNetYields(int aiYields[NUM_YIELD_TYPES], int* aiProducedYi
 						 eYieldConsumed = eYieldConsumed2;
 					}
 					CvWString szBuffer = gDLL->getText("TXT_KEY_NOT_ENOUGH_RAW", getNameKey(),GC.getYieldInfo(eYieldConsumed).getChar(), GC.getYieldInfo(eYieldProduced).getChar());
-					gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_DEAL_CANCELLED", MESSAGE_TYPE_MINOR_EVENT, GC.getYieldInfo(eYieldConsumed).getButton(), COLOR_RED, true, true);
+
+					InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+					if (GC.getOUT_OF_RESOURECES_MESSAGES() == 0)
+					{
+						messageType = MESSAGE_TYPE_LOG_ONLY;
+					}
+
+					gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_DEAL_CANCELLED", messageType, GC.getYieldInfo(eYieldConsumed).getButton(), COLOR_RED, true, true);
 				}
 			}
 		}
@@ -7002,7 +7030,13 @@ void CvCity::popOrder(int iNum, bool bFinish, bool bChoose)
 			szBuffer += gDLL->getText("TXT_KEY_MISC_WORK_HAS_BEGUN", getProductionNameKey());
 		}
 
-		gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), szSound, MESSAGE_TYPE_MINOR_EVENT, szIcon, COLOR_WHITE, true, true);
+		InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+		if (GC.getCONSTRUCTION_MESSAGES() == 0)
+		{
+			messageType = MESSAGE_TYPE_LOG_ONLY;
+		}
+
+		gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), szSound, messageType, szIcon, COLOR_WHITE, true, true);
 	}
 
 	setAutoThresholdCache(); // transport feeder - Nightinggale
@@ -7366,7 +7400,13 @@ void CvCity::doGrowth()
 				// WTP, ray, making this error save to prevent negative Storage bug - END
 			}
 
-			gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), gDLL->getText("TXT_KEY_CITY_GROWTH", getNameKey()), coord(), "AS2D_POSITIVE_DINK", MESSAGE_TYPE_INFO, GC.getYieldInfo(YIELD_FOOD).getButton(), COLOR_GREEN, true, true);
+			InterfaceMessageTypes messageType = MESSAGE_TYPE_INFO;
+			if (GC.getCITY_GROWTH_MESSAGES() == 0)
+			{
+				messageType = MESSAGE_TYPE_LOG_ONLY;
+			}
+
+			gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), gDLL->getText("TXT_KEY_CITY_GROWTH", getNameKey()), coord(), "AS2D_POSITIVE_DINK", messageType, GC.getYieldInfo(YIELD_FOOD).getButton(), COLOR_GREEN, true, true);
 
 
 			// ONEVENT - City growth
@@ -7508,10 +7548,17 @@ void CvCity::doYields()
 				iTotalProfitFromDomesticMarket = iTotalProfitFromDomesticMarket + iProfit;
 			}
 		}
-		if (iTotalProfitFromDomesticMarket != 0 && GC.getDOMESTIC_SALES_MESSAGES() == 1)
+		if (iTotalProfitFromDomesticMarket != 0)
 		{
 			CvWString szBuffer = gDLL->getText("TXT_KEY_GOODS_DOMESTIC_SOLD", getNameKey(), iTotalProfitFromDomesticMarket);
-			gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), NULL, MESSAGE_TYPE_MINOR_EVENT, NULL, COLOR_WHITE, true, true);
+
+			InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+			if (GC.getDOMESTIC_SALES_MESSAGES() == 0)
+			{
+				messageType = MESSAGE_TYPE_LOG_ONLY;
+			}
+
+			gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), NULL, messageType, NULL, COLOR_WHITE, true, true);
 		}
 	}
 	// R&R, ray, adjustment Domestic Markets, END
@@ -7562,7 +7609,14 @@ void CvCity::doYields()
 
 							if (iTurns == 2 || iTurns == 1) {
 								CvWString szBuffer = gDLL->getText("TXT_KEY_MISC_STUDENT_ALMOST_GRADUATED", iTurns, getNameKey(), GC.getBuildingInfo(eSchoolBuilding).getTextKeyWide());
-								gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_CULTUREEXPANDS", MESSAGE_TYPE_MINOR_EVENT, GC.getYieldInfo(YIELD_EDUCATION).getButton(), COLOR_WHITE, true, true);
+
+								InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+								if (GC.getSTUDENDS_MESSAGES() == 0)
+								{
+									messageType = MESSAGE_TYPE_LOG_ONLY;
+								}
+
+								gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_CULTUREEXPANDS", messageType, GC.getYieldInfo(YIELD_EDUCATION).getButton(), COLOR_WHITE, true, true);
 
 							}
 							// TAC - Messages - Ray - END
@@ -12064,7 +12118,14 @@ bool CvCity::educateStudent(int iUnitId, UnitTypes eUnit)
 	if(eSchoolBuilding != NO_BUILDING)
 	{
 		CvWString szBuffer = gDLL->getText("TXT_KEY_MISC_STUDENT_GRADUATED", GC.getUnitInfo(eUnit).getTextKeyWide(), getNameKey(), GC.getBuildingInfo(eSchoolBuilding).getTextKeyWide());
-		gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_CULTUREEXPANDS", MESSAGE_TYPE_MINOR_EVENT, GC.getYieldInfo(YIELD_EDUCATION).getButton(), COLOR_WHITE, true, true);
+
+		InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+		if (GC.getSTUDENDS_MESSAGES() == 0)
+		{
+			messageType = MESSAGE_TYPE_LOG_ONLY;
+		}
+
+		gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), "AS2D_CULTUREEXPANDS", messageType, GC.getYieldInfo(YIELD_EDUCATION).getButton(), COLOR_WHITE, true, true);
 	}
 
 	// Teacher List - start - Nightinggale
@@ -14011,7 +14072,14 @@ void CvCity::doEntertainmentBuildings()
 		OOS_LOG_3("Entertainment building", CvString(getName()).c_str(), iGoldthroughCulture);
 		GET_PLAYER(getOwnerINLINE()).changeGold(iGoldthroughCulture);
 		CvWString szBuffer = gDLL->getText("TXT_KEY_GOLD_BY_ENTERTAINMENT", GC.getBuildingInfo(highestLevelEntertainmentBuilding).getDescription(), getNameKey(), iGoldthroughCulture);
-		gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), NULL, MESSAGE_TYPE_MINOR_EVENT, NULL, COLOR_WHITE, true, true);
+
+		InterfaceMessageTypes messageType = MESSAGE_TYPE_MINOR_EVENT;
+		if (GC.getGOLD_BY_ENTERTAINMEN_MESSAGES() == 0)
+		{
+			messageType = MESSAGE_TYPE_LOG_ONLY;
+		}
+
+		gDLL->UI().addPlayerMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer, coord(), NULL, messageType, NULL, COLOR_WHITE, true, true);
 	}
 }
 // R&R, ray, Entertainment Buildings - END
